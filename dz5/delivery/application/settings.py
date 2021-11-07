3061eccffd96c869c0def9ad6e556d9d86904d55
Пудовкин Дмитрий <p134d@mail.ru>
2021-11-07 21:30:22 +0300

    dz5 update settings.py

--- dz5/delivery/application/settings.py
+++ dz5/delivery/application/settings.py
@@ -79,17 +79,6 @@ WSGI_APPLICATION = 'application.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'delivery',
-#         'USER': 'django_db',
-#         'PASSWORD': 'password',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': local_settings.database_engine,
