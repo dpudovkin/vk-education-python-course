3ecda57cb88c3f0c5c4781df655f24af9bfe42da
Пудовкин Дмитрий <p134d@mail.ru>
2021-11-11 23:43:34 +0300

    start admin model

--- dz5-dz6/delivery/orders/admin.py
+++ dz5-dz6/delivery/orders/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlencode
 
 from orders.models import Order, Address
 
-admin.site.register(Order)
-admin.site.register(Address)
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "cost", "comment", "status", "view_performer", "base_award")
+    list_filter = ("status",)
+
+    def view_performer(self, obj):
+        employee = obj.performer_id
+        url = (
+            reverse("admin:employees_employee_changelist")
+            + "?"
+            + urlencode({"employees__id": f"{obj.id}"})
+        )
+        return format_html('<a href="{}">{}</a>', url, employee)
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "longitude", "latitude")
+
